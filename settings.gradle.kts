ebe92312ed58128c43fc2f937e14692ea1f6db1a
Anton Shestak <ashestak@icerockdev.com>
2021-07-16 11:28:28 +0700

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2021 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+enableFeaturePreview("VERSION_CATALOGS")
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        google()
+
+        jcenter {
+            content {
+                includeGroup("org.jetbrains.kotlinx")
+            }
+        }
+    }
+}
+includeBuild("web3-build-logic")
+
+include(":web3")
+include(":sample:android-app")
+include(":sample:mpp-library")
