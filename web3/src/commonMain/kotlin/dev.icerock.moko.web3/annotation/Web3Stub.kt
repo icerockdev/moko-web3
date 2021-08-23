3f6d7103c1030c94a4cea24ccdbd284290cb97cb
Alex Sokol <sokolsokolinskiy@gmail.com>
2021-08-23 10:16:49 +0300

    #31: OptIn added to SmartContract#write

---
+++ web3/src/commonMain/kotlin/dev.icerock.moko.web3/annotation/Web3Stub.kt
@@ -0,0 +1,7 @@
+package dev.icerock.moko.web3.annotation
+
+@RequiresOptIn(
+    message = "STUB: This method will be implemented in the future",
+    level = RequiresOptIn.Level.ERROR
+)
+annotation class Web3Stub
