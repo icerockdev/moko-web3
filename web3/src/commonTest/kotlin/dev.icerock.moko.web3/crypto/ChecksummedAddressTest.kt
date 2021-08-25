812dfa6be55b1930425186d0eccba3d4aa33a323
Alex Sokol <sokolsokolinskiy@gmail.com>
2021-08-25 15:04:43 +0300

    #38: test is valid added

--- web3/src/commonTest/kotlin/dev.icerock.moko.web3/crypto/ChecksummedAddressTest.kt
+++ web3/src/commonTest/kotlin/dev.icerock.moko.web3/crypto/ChecksummedAddressTest.kt
@@ -3,6 +3,7 @@ package dev.icerock.moko.web3.crypto
 import dev.icerock.moko.web3.ContractAddress
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class ChecksummedAddressTest {
     @Test
@@ -12,5 +13,15 @@ class ChecksummedAddressTest {
             expected = contractAddress,
             actual = contractAddress.checksummed
         )
+    }
+
+    @Test
+    fun `test is valid`() {
+        val contractAddress = ContractAddress(value = "0xC9472182861faD74b6810cf58C792501F8837473")
+        assertTrue(contractAddress.isValid)
+        val contractAddressNotChecksummedUppercase = ContractAddress(value = contractAddress.value.uppercase())
+        assertTrue(contractAddressNotChecksummedUppercase.isValid)
+        val contractAddressNotChecksummedLowercase = ContractAddress(value = contractAddress.value.lowercase())
+        assertTrue(contractAddressNotChecksummedLowercase.isValid)
     }
 }
