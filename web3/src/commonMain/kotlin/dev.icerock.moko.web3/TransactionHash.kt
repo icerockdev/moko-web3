8d0c0ace6b8c031b8754e462086b255a10d1f0a8
y9san9 <y9san9@gmail.com>
2021-10-14 14:57:10 +0300

    #22: little fix

--- web3/src/commonMain/kotlin/dev.icerock.moko.web3/TransactionHash.kt
+++ web3/src/commonMain/kotlin/dev.icerock.moko.web3/TransactionHash.kt
@@ -6,10 +6,26 @@ package dev.icerock.moko.web3
 
 import dev.icerock.moko.web3.hex.Hex32String
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 
-@Serializable
+@Serializable(with = TransactionHash.Serializer::class)
 class TransactionHash(private val value: String) : Hex32String by Hex32String(value) {
-    override fun toString() = withoutPrefix 
+    override fun toString() = withoutPrefix
     override fun hashCode(): Int = withoutPrefix.hashCode()
     override fun equals(other: Any?): Boolean = other is TransactionHash && other.withoutPrefix == withoutPrefix
+
+    @kotlinx.serialization.Serializer(forClass = TransactionHash::class)
+    object Serializer {
+        override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor(
+            serialName = "dev.icerock.mokoWeb3.TransactionHash",
+            kind = PrimitiveKind.STRING
+        )
+
+        override fun deserialize(decoder: Decoder): TransactionHash = TransactionHash(decoder.decodeString())
+        override fun serialize(encoder: Encoder, value: TransactionHash) = encoder.encodeString(value.value)
+    }
 }
