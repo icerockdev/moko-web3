a2415ff0662214716ea925258f07893774e5b35a
y9san9 <y9san9@gmail.com>
2021-11-13 00:19:03 +0300

    #77: transformers fixed

--- web3/src/commonMain/kotlin/dev.icerock.moko.web3/hex/Transformers.kt
+++ web3/src/commonMain/kotlin/dev.icerock.moko.web3/hex/Transformers.kt
@@ -4,19 +4,25 @@
 
 package dev.icerock.moko.web3.hex
 
-val Hex8String.asHex8 get() = convertToSizedHex(Hex8String)
-val Hex16String.asHex16 get() = convertToSizedHex(Hex16String)
-val Hex20String.asHex20 get() = convertToSizedHex(Hex20String)
-val HexString.asHex32 get() = convertToSizedHex(Hex32String)
-val Hex64String.asHex64 get() = convertToSizedHex(Hex64String)
-val Hex128String.asHex128 get() = convertToSizedHex(Hex128String)
-val Hex256String.asHex256 get() = convertToSizedHex(Hex256String)
+fun HexString.fillToHex8() = fillToSizedHex(Hex8String)
+fun HexString.fillToHex16() = fillToSizedHex(Hex16String)
+fun HexString.fillToHex20() = fillToSizedHex(Hex20String)
+fun HexString.fillToHex32() = fillToSizedHex(Hex32String)
+fun HexString.fillToHex64() = fillToSizedHex(Hex64String)
+fun HexString.fillToHex128() = fillToSizedHex(Hex128String)
+fun HexString.fillToHex256() = fillToSizedHex(Hex256String)
 
-fun <T> HexString.convertToSizedHex(typeclass: HexString.SizedFactory<T>): T {
+/**
+ * This function adds leading zeros, so the original hex
+ * will be padded to required size.
+ *
+ * HexString("0x10").fillToHex8() - Hex8String("0x0000000000000010")
+ */
+fun <T> HexString.fillToSizedHex(typeclass: HexString.SizedFactory<T>): T {
     val currentSize = withoutPrefix.length / 2
 
     if (currentSize > typeclass.size)
-        error("This hex string has more bytes than the target one")
+        error("This hex string already has more bytes than the target one")
     val isStrictlyValid = withoutPrefix.length % 2 == 0
 
     return typeclass.createInstance(value = buildString {
