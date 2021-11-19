1efee46a81a51d08bdc95408bcb71465194fb8bd
Denis Chornyy <dchornyy@icerockdev.com>
2021-11-19 18:13:40 +0700

    #86: add bytes encoder

---
+++ web3/src/commonMain/kotlin/dev.icerock.moko.web3/contract/BytesParam.kt
@@ -0,0 +1,22 @@
+package dev.icerock.moko.web3.contract
+
+import com.soywiz.kbignum.bi
+import dev.icerock.moko.web3.contract.MethodEncoder.PART_SIZE
+
+class BytesParam : DynamicEncoder<ByteArray> {
+    override fun encode(item: ByteArray): ByteArray {
+        val sizeData = UInt256Param.encode(item.size.bi)
+        return item
+            .asIterable()
+            .chunked(PART_SIZE)
+            .map {
+                it.toByteArray() + ByteArray(PART_SIZE - it.size)
+            }.fold(initial = sizeData) { acc, bytes ->
+                acc + bytes
+            }
+    }
+
+    override fun decode(source: ByteArray): ByteArray {
+        TODO("moko-web3 does not support decoding dynamic params yet")
+    }
+}
